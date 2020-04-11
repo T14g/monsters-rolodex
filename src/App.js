d138384638654148a33f90dece80956c788b0f9f
t14g <ottiagos@gmail.com>
2020-04-11 16:25:05 -0300

    Card List no css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();//permite usar o state, neste exemplo
+
+    this.state = {
+      monsters : []
+    }
+  }
+
+  componentDidMount() {
+    //api request return a promisse
+    //then promisse then another promisse
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters : users }))
+  }
+
+  render(){
+    return ( 
+      <div className="App">
+        <CardList>Aleluja!!</CardList>
+        {this.state.monsters.map(monster => (
+          <h2 key={monster.id}>{monster.name}</h2>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
