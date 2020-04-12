7a7a480239ab3d98b131197b634638c2d510dc31
t14g <ottiagos@gmail.com>
2020-04-12 18:52:31 -0300

    Updates

---
+++ precache-manifest.a348f55b5e186e5d89f75512e1a83327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046da541e6bfcdf33be88b720ae5a33c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b04c7f3cd9726c6eeaed",
+    "url": "/monsters-rolodex/static/css/main.ba035f7f.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b04c7f3cd9726c6eeaed",
+    "url": "/monsters-rolodex/static/js/main.3e077ddd.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
